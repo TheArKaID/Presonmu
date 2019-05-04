d1bc8edbf05c5ae55518584f40248000257d3033
Arifia Kasastra R <arka.progammer@gmail.com>
2019-05-04 20:19:22 +0700

    Add CheckLocationPermission to ask for Permission

--- app/src/main/java/id/ac/umy/unires/mh/Welcome.java
+++ app/src/main/java/id/ac/umy/unires/mh/Welcome.java
@@ -1,6 +1,12 @@
 package id.ac.umy.unires.mh;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,27 +14,63 @@ import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
 
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     Button checkingBtn;
+    AlertDialog.Builder showAskPermission;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
         checkingBtn = findViewById(R.id.CheckingBtn);
+        showAskPermission = new AlertDialog.Builder(this);
 
         checkingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                CheckLocationPermission();
                 SharedPreferences pref = getApplicationContext().getSharedPreferences("id.ac.umy.unires.mh.DATA_DIRI", MODE_PRIVATE);
                 SharedPreferences.Editor prefEdit = pref.edit();
-                if(pref.getString("uid", null) != null && pref.getString("pass", null) != null){
-                    //TODO : Intent ke halaman awal melalui login dengan data yang disimpan
-                } else{
+                if (pref.getString("uid", null) != null && pref.getString("pass", null) != null) {
+                    // TODO : Intent ke halaman awal melalui login dengan data yang disimpan
+                } else {
                     // TODO : Intent ke halaman login
                 }
 
                 prefEdit.apply();
             }
         });
+    }
+
+    private void CheckLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(Welcome.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Welcome.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            showAskPermission.setTitle("Izin dibutuhkan")
+                    .setMessage("Aplikasi ini harus memiliki izin untuk mengakses lokasi anda.")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(Welcome.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                            ActivityCompat.requestPermissions(Welcome.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                        }
+                    }).show();
+        }
+        else {
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            else{
+                //CheckLocationPermission();
+            }
+        }
     }
 }
