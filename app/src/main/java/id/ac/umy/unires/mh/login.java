5f0520948f35f59f04734f12e396288d51940fc3
Arifia Kasastra R <arka.progammer@gmail.com>
2019-05-04 21:24:14 +0700

    Create Field for some component and create onClick on Button Login

--- app/src/main/java/id/ac/umy/unires/mh/login.java
+++ app/src/main/java/id/ac/umy/unires/mh/login.java
@@ -2,12 +2,39 @@ package id.ac.umy.unires.mh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
 
+    EditText emailET;
+    EditText passwordET;
+    Button login;
+
+    String email;
+    String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailET = findViewById(R.id.EmailET);
+        passwordET = findViewById(R.id.PassET);
+        login = findViewById(R.id.LoginBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailET.getText().toString();
+                password = passwordET.getText().toString();
+
+                Login(email, password);
+            }
+        });
+    }
+
+    private void Login(String email, String password) {
+
     }
 }
