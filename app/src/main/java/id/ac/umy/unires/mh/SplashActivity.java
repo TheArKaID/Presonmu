28a167b0b9f08caac4185471c35bdb992374f4ad
Arifia Kasastra R <arka.progammer@gmail.com>
2019-05-04 09:38:10 +0700

    Intent to the Welcome Activity after 5 seconds

--- app/src/main/java/id/ac/umy/unires/mh/SplashActivity.java
+++ app/src/main/java/id/ac/umy/unires/mh/SplashActivity.java
@@ -1,5 +1,6 @@
 package id.ac.umy.unires.mh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,34 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+
+                    sleep(5000);
+
+                } catch (Exception e){
+
+                    e.printStackTrace();
+
+                } finally {
+
+                    Intent welcomeIntent = new Intent(SplashActivity.this, Welcome.class);
+                    startActivity(welcomeIntent);
+
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        finish();
     }
 }
