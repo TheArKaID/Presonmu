67c485be0abf9bd260062beb0e5dc11e37316826
Arifia Kasastra R <arka.progammer@gmail.com>
2019-05-04 12:50:14 +0700

    Add SharedPreferences to check the available data login

--- app/src/main/java/id/ac/umy/unires/mh/Welcome.java
+++ app/src/main/java/id/ac/umy/unires/mh/Welcome.java
@@ -1,7 +1,9 @@
 package id.ac.umy.unires.mh;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
@@ -13,5 +15,20 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         checkingBtn = findViewById(R.id.CheckingBtn);
+
+        checkingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("id.ac.umy.unires.mh.DATA_DIRI", MODE_PRIVATE);
+                SharedPreferences.Editor prefEdit = pref.edit();
+                if(pref.getString("uid", null) != null && pref.getString("pass", null) != null){
+                    //TODO : Intent ke halaman awal melalui login dengan data yang disimpan
+                } else{
+                    // TODO : Intent ke halaman login
+                }
+
+                prefEdit.apply();
+            }
+        });
     }
 }
