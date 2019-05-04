cf15aaaea027e6649a1f5aa6342e62f2f60783ca
Arifia Kasastra R <arka.progammer@gmail.com>
2019-05-04 09:47:57 +0700

    Set id of Masuk Button

--- app/src/main/java/id/ac/umy/unires/mh/Welcome.java
+++ app/src/main/java/id/ac/umy/unires/mh/Welcome.java
@@ -2,12 +2,16 @@ package id.ac.umy.unires.mh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
 
+    Button checkingBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        checkingBtn = findViewById(R.id.CheckingBtn);
     }
 }
