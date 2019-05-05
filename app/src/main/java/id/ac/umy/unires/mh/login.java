843abcf53d4efb56e1ebb305c2e4be3476f7192c
Arifia Kasastra R <arka.progammer@gmail.com>
2019-05-05 14:28:25 +0700

    Make Login FUnction with email and password parameter

--- app/src/main/java/id/ac/umy/unires/mh/login.java
+++ app/src/main/java/id/ac/umy/unires/mh/login.java
@@ -1,14 +1,24 @@
 package id.ac.umy.unires.mh;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class login extends AppCompatActivity {
 
@@ -18,6 +28,10 @@ public class login extends AppCompatActivity {
 
     String email;
     String password;
+
+    Map dataUser;
+
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +40,8 @@ public class login extends AppCompatActivity {
         emailET = findViewById(R.id.EmailET);
         passwordET = findViewById(R.id.PassET);
         login = findViewById(R.id.LoginBtn);
+        db = FirebaseFirestore.getInstance();
+        dataUser = new HashMap();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,10 +55,26 @@ public class login extends AppCompatActivity {
     }
 
     private void Login(String email, String password) {
-        if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password)){
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
             Toast.makeText(this,"Harap Masukkan email dan password anda", Toast.LENGTH_LONG).show();
         }
         else {
+            db.collection("users").whereEqualTo("email", email).whereEqualTo("password", password)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                if(!task.getResult().isEmpty()){
+                                    QuerySnapshot qs = task.getResult();
+                                    List<DocumentSnapshot> qds = qs.getDocuments();
+                                    dataUser.putAll(qds.get(0).getData());
+                                }
+                            } else {
+                                Log.w("FragmentActivity", "Error getting documents.", task.getException());
+                            }
+                        }
+                    });
 
         }
     }
