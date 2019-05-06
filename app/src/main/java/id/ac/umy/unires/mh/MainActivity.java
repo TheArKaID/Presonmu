d4933fdb097684b5b31c292559ff37ded5df2fb2
Arifia Kasastra R <arka.progammer@gmail.com>
2019-05-06 23:17:25 +0700

    Set drawer_menu_header as headerLayout and change the background color to PrimaryDark

--- app/src/main/java/id/ac/umy/unires/mh/MainActivity.java
+++ app/src/main/java/id/ac/umy/unires/mh/MainActivity.java
@@ -1,13 +1,39 @@
 package id.ac.umy.unires.mh;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.activity_main);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
