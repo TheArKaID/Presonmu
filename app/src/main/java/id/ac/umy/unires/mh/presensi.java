70ff56bb273a90d694c3d57a2045d5ec596694e8
Arifia Kasastra R <arka.progammer@gmail.com>
2019-06-08 18:41:56 +0700

    Initiate element in activity_presensi.xml

--- app/src/main/java/id/ac/umy/unires/mh/presensi.java
+++ app/src/main/java/id/ac/umy/unires/mh/presensi.java
@@ -7,12 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class presensi extends Fragment {
 
+    TextView haridantanggal, shiftke;
+    Button absen;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_presensi, container, false);
+
+        View v = inflater.inflate(R.layout.activity_presensi, container, false);
+
+        haridantanggal = v.findViewById(R.id.HaridanTanggal);
+        shiftke = v.findViewById(R.id.ShiftKe);
+        absen = v.findViewById(R.id.Absen);
+
+        return v;
     }
 }
