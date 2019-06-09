33863893710847cb3ec91f2232800f2fe8ed8202
Arifia Kasastra R <arka.progammer@gmail.com>
2019-06-09 11:31:57 +0700

    Add the other Server APIs URL

--- app/src/main/java/id/ac/umy/unires/mh/Utils/ServerAPI.java
+++ app/src/main/java/id/ac/umy/unires/mh/Utils/ServerAPI.java
@@ -4,4 +4,7 @@ public class ServerAPI {
     public static final String BASE_URL = "https://presonmu.000webhostapp.com/";
     public static final String LOGIN_URL = BASE_URL+"login.php";
     public static final String DATETIME_URL = BASE_URL+"timeabsen.php";
+    public static final String ABOUT_URL = BASE_URL+"about.php";
+    public static final String ABSEN_URL = BASE_URL+"absen.php";
+    public static final String RIWAYAT_URL = BASE_URL+"riyawat.php";
 }
