86a4f5ef00cbe58648e8a8fcb023cd3abc5795da
Arifia Kasastra R <arka.progammer@gmail.com>
2019-06-18 01:05:38 +0700

    Add RIWAYAT_URL API data and add the string for activity_riwayat.xml

--- app/src/main/java/id/ac/umy/unires/mh/Utils/ServerAPI.java
+++ app/src/main/java/id/ac/umy/unires/mh/Utils/ServerAPI.java
@@ -6,7 +6,7 @@ public class ServerAPI {
     public static final String DATETIME_URL = BASE_URL+"timeabsen.php";
     public static final String ABOUT_URL = BASE_URL+"about.php";
     public static final String ABSEN_URL = BASE_URL+"absen.php";
-    public static final String RIWAYAT_URL = BASE_URL+"riyawat.php";
+    public static final String CEKRIWAYAT_URL = BASE_URL+"cekriwayat.php";
     public static final String CEKABSEN_URL = BASE_URL+"cekabsen.php";
     public static final String MYHOME_URL = BASE_URL+"home.php";
     public static final String STATUSHOME_URL = BASE_URL+"allstatus.php";
