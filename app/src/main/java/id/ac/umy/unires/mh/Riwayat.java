8181c066ba7e393e740fdc6415adecf4e28d2e50
Arifia Kasastra R <arka.progammer@gmail.com>
2019-06-18 01:07:12 +0700

    Create method loadData() to take data from database

--- app/src/main/java/id/ac/umy/unires/mh/Riwayat.java
+++ app/src/main/java/id/ac/umy/unires/mh/Riwayat.java
@@ -8,11 +8,69 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import static id.ac.umy.unires.mh.MainActivity.email;
+import static id.ac.umy.unires.mh.Utils.ServerAPI.CEKRIWAYAT_URL;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Riwayat extends Fragment {
+
+    BarChart barChart;
+
+    ArrayList<BarEntry> shift1;
+    ArrayList<BarEntry> shift2;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_riwayat, container, false);
+        View v = inflater.inflate(R.layout.activity_riwayat, container, false);
+
+        barChart = v.findViewById(R.id.mp_barchart);
+
+        shift1 = new ArrayList<>();
+        shift2 = new ArrayList<>();
+
+        loadData(email);
+
+        return v;
+    }
+
+    private void loadData(final String email) {
+        StringRequest request = new StringRequest(Request.Method.POST, CEKRIWAYAT_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    HashMap<String, String> params = new HashMap<>();
+
+                    params.put("email", email);
+
+                    return params;
+                }
+        };
+
+        Volley.newRequestQueue(getContext()).add(request);
     }
 }
