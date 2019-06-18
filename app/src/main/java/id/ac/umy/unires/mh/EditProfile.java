51304a7a18a4286afcb3ef71850d83a406eda344
Arifia Kasastra R <arka.progammer@gmail.com>
2019-06-18 14:23:40 +0700

    Create some variables in EditProfile.java

--- app/src/main/java/id/ac/umy/unires/mh/EditProfile.java
+++ app/src/main/java/id/ac/umy/unires/mh/EditProfile.java
@@ -2,12 +2,23 @@ package id.ac.umy.unires.mh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class EditProfile extends AppCompatActivity {
+
+    ImageView iv_editFoto;
+    EditText et_nama, et_status, et_password, et_newPass, et_newRePass;
+    Button btn_simpan;
+
+    String nama, status, password, newPass, newRePass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+
     }
 }
