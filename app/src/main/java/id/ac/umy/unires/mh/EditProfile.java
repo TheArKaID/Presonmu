3f9179854e92059ce6a56ee04aa3448ef5fd0ac1
Arifia Kasastra R <arka.progammer@gmail.com>
2019-06-18 14:57:57 +0700

    Add onClickListener at ImageView in EditProfile.java, and method to chose an image from gallery (MANIFEST PERMISSION ADDED)

--- app/src/main/java/id/ac/umy/unires/mh/EditProfile.java
+++ app/src/main/java/id/ac/umy/unires/mh/EditProfile.java
@@ -1,11 +1,18 @@
 package id.ac.umy.unires.mh;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -14,6 +21,10 @@ public class EditProfile extends AppCompatActivity {
     Button btn_simpan;
 
     String nama, status, password, newPass, newRePass;
+
+    private final int REQUEST_IMAGE_FROM_GALLERY = 200;
+
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +43,33 @@ public class EditProfile extends AppCompatActivity {
 
         et_nama.setText(bundle.getString("nama"));
         et_status.setText(bundle.getString("status"));;
+
+        iv_editFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pilihGambar();
+            }
+        });
+    }
+
+    private void pilihGambar() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, REQUEST_IMAGE_FROM_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUEST_IMAGE_FROM_GALLERY && data != null){
+            Uri image = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), image);
+                iv_editFoto.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
