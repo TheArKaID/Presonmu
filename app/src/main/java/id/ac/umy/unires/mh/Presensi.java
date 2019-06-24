25477fb9932a43075bce0637f80cfee39e3b9fbb
Arifia Kasastra R <arka.progammer@gmail.com>
2019-06-25 01:04:14 +0700

    Add and initialize MapView and GoogleMap in Presensi.java and implement and override their needed methods

--- app/src/main/java/id/ac/umy/unires/mh/Presensi.java
+++ app/src/main/java/id/ac/umy/unires/mh/Presensi.java
@@ -1,11 +1,14 @@
 package id.ac.umy.unires.mh;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,6 +25,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,7 +40,7 @@ import java.util.Map;
 import static id.ac.umy.unires.mh.MainActivity.*;
 import static id.ac.umy.unires.mh.Utils.ServerAPI.*;
 
-public class Presensi extends Fragment {
+public class Presensi extends Fragment implements OnMapReadyCallback {
 
     TextView haridantanggal, shiftke;
     Button absen;
@@ -40,6 +48,9 @@ public class Presensi extends Fragment {
     String haridantanggalnya, shift;
 
     ProgressDialog checkBar;
+
+    MapView mapView;
+    GoogleMap map;
 
     @Nullable
     @Override
@@ -50,6 +61,10 @@ public class Presensi extends Fragment {
         haridantanggal = v.findViewById(R.id.HaridanTanggal);
         shiftke = v.findViewById(R.id.ShiftKe);
         absen = v.findViewById(R.id.Absen);
+
+        mapView = v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
         loadData();
 
@@ -63,7 +78,7 @@ public class Presensi extends Fragment {
         return v;
     }
 
-    private void loadData(){
+    private void loadData() {
         LoadingBarCheck();
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, DATETIME_URL, null,
                 new Response.Listener<JSONObject>() {
@@ -75,13 +90,13 @@ public class Presensi extends Fragment {
                             haridantanggal.setText(haridantanggalnya);
                             shiftke.setText(String.format("Shift ke %s", shift));
 
-                            if(!response.getBoolean("absenable")){
+                            if (!response.getBoolean("absenable")) {
                                 absen.setClickable(response.getBoolean("absenable"));
                                 absen.setBackgroundColor(getResources().getColor(R.color.colorFalse));
-                            }else{
+                            } else {
                                 cekAbsen(email);
                             }
-                            Log.d("Log, ", "Response => "+response.toString());
+                            Log.d("Log, ", "Response => " + response.toString());
                             checkBar.dismiss();
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -91,7 +106,7 @@ public class Presensi extends Fragment {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.d("Error Volley, ", "onErrorResponse => "+error.getMessage());
+                        Log.d("Error Volley, ", "onErrorResponse => " + error.getMessage());
                     }
                 });
         Volley.newRequestQueue(getContext()).add(request);
@@ -102,7 +117,7 @@ public class Presensi extends Fragment {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        if(response.equals("Done")){
+                        if (response.equals("Done")) {
                             didAbsen();
                             Log.d("Response cek Absen => ", response);
                         }
@@ -113,7 +128,7 @@ public class Presensi extends Fragment {
                     public void onErrorResponse(VolleyError error) {
                         Log.d("Error =>", error.getMessage());
                     }
-                }){
+                }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<>();
@@ -135,7 +150,7 @@ public class Presensi extends Fragment {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        if(response.equals("Sukses")){
+                        if (response.equals("Sukses")) {
                             checkBar.dismiss();
                             notif.setTitle("Sukses")
                                     .setCancelable(false)
@@ -146,7 +161,7 @@ public class Presensi extends Fragment {
                                             loadData();
                                         }
                                     }).show();
-                        } else{
+                        } else {
                             checkBar.dismiss();
                             notif.setTitle("Gagal")
                                     .setMessage("Coba Lagi")
@@ -166,7 +181,7 @@ public class Presensi extends Fragment {
                         checkBar.dismiss();
                         Log.d("onErrorResponse", error.getMessage());
                     }
-                }){
+                }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<>();
@@ -195,5 +210,45 @@ public class Presensi extends Fragment {
         checkBar.setCanceledOnTouchOutside(false);
         checkBar.setCancelable(false);
         checkBar.show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+//        map.getUiSettings().setMyLocationButtonEnabled(false);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, Welcome.MY_PERMISSIONS_REQUEST_LOCATION);
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        LatLng latLng = new LatLng(-7.81012,110.3220527);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
