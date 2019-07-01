fdf218f52c19614896c66aa24e8f3093cd3d0cb2
Arifia Kasastra R <arka.progammer@gmail.com>
2019-07-01 08:42:41 +0700

    Change ping url when isInternetWorking checking in Login.java

--- app/src/main/java/id/ac/umy/unires/mh/Login.java
+++ app/src/main/java/id/ac/umy/unires/mh/Login.java
@@ -144,7 +144,7 @@ public class Login extends AppCompatActivity {
     public boolean isInternetWorking() {
         boolean success = false;
         try {
-            URL url = new URL("https://google.com");
+            URL url = new URL("https://google.co.id");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setConnectTimeout(10000);
             connection.connect();
