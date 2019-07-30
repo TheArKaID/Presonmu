0a14bbf480eb81be9a34fd6af93aaa435b12e3a5
Arifia Kasastra R <arka.progammer@gmail.com>
2019-07-31 06:21:49 +0700

    Add fromhetml format for responses in About.java

--- app/src/main/java/id/ac/umy/unires/mh/fragment/About.java
+++ app/src/main/java/id/ac/umy/unires/mh/fragment/About.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -51,7 +52,7 @@ public class About extends Fragment {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            about.setText(response.getString("informasi"));
+                            about.setText(Html.fromHtml(response.getString("informasi")));
                             version.setText(String.format("Version %s", response.getString("version")));
                         } catch (JSONException e) {
                             e.printStackTrace();
